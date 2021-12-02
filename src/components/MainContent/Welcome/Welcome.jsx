cc974b20d0ee0c8a2aee078c502cb096e3f023b2
Louis Récopé <louisdtb@gmail.com>
2021-12-03 00:51:40 +0100

    played with framer motion

--- src/components/MainContent/Welcome/Welcome.jsx
+++ src/components/MainContent/Welcome/Welcome.jsx
@@ -1,25 +1,36 @@
-import React from "react";
+import { React, useState, useEffect } from "react";
 import { motion } from "framer-motion";
 import { pageTransition } from "../../../pageTransition";
 
 const Welcome = () => {
+  // const [transition, setTransition] = useState(true);
+
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     setTransition(false);
+  //   }, 1000);
+  // }, []);
+
   const animate = {
-    hidden: { y: "100%" },
-    show: { y: "-100%" },
+    hidden: { y: "0" },
+    show: {
+      y: "-100%",
+      transition: { duration: 1, ease: [0.85, 0, 0.15, 1] },
+    },
     exit: { y: "100%" },
   };
 
   return (
-    <>
+    <div className="welcome-wrapper">
       <motion.div
         className="transition"
         exit="hidden"
         animate="show"
         initial="exit"
         variants={animate}
-      ></motion.div>
+      />
       <div className="welcome">Welcome to Trillo!</div>
-    </>
+    </div>
   );
 };
 
